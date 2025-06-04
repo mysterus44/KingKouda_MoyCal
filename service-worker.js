d38e9ead934d8e3a06e3763996a2cc280a9a4c95
kingkouda <120647091+mysterus44@users.noreply.github.com>
2025-06-04 12:54:49 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,28 @@
-const CACHE_NAME = "calculateur-moyenne-cache-v1";
+const CACHE_NAME = "moycal-v1";
 const urlsToCache = [
   "/",
   "/index.html",
-  "/manifest.json",
+  "/index1.html",
+  "/index2.html",
   "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/icons/icon-512.png",
+  "/whats.png",
+  "/manifest.json",
+  // Ajoute ici tout autre fichier JS, CSS, image, etc.
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
